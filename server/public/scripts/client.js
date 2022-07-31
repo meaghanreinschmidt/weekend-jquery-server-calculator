8ea437fa81f92a93647229f6620be0a782bf69b0
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-07-31 00:37:50 -0500

    push input field values and operator to array on client side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,35 @@
 $(document).ready(onReady);
 
 function onReady() {
-    
+    $('body').on('click', '#calculator-operation', setFirstHalf)
+    $('body').on('click', '#calculator-equal', setSecondHalf)
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+let operationHistory = [];
+
+function setFirstHalf() {
+    let firstNumber = $('#first-number').val();
+    console.log('this is the first number', firstNumber);
+    operator = $(this).data('value');
+    console.log('this is the operator', operator);
+}
+
+function setSecondHalf() {
+    let firstNumber = $('#first-number').val();
+    let secondNumber = $('#second-number').val();
+    console.log('this is the second number', secondNumber);
+    let operation = {
+        firstNumber: firstNumber,
+        operator: operator,
+        secondNumber: secondNumber
+    };
+    console.log(operation);
+    operationHistory.push(operation);
+    console.log(operationHistory);
+    $('#first-number').val();
+    $('#second-number').val();
 }
